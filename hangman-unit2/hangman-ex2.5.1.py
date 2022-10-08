1e941d81316f6ac335fb30f00fdbed97fc9ba60f
idorombaut <76433005+idorombaut@users.noreply.github.com>
2022-10-08 21:32:27 +0300

    Unit 2

---
+++ hangman-unit2/hangman-ex2.5.1.py
@@ -0,0 +1,13 @@
+import random
+HANGMAN_ASCII_ART = """Welcome to the game Hangman
+  _    _                                         
+ | |  | |                                        
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                      __/ |                      
+                     |___/
+"""
+MAX_TRIES = random.randint(5, 10)
+print(HANGMAN_ASCII_ART, MAX_TRIES)      
