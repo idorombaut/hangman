0ef874b0bbe542221666863a182ba509417914fe
idorombaut <76433005+idorombaut@users.noreply.github.com>
2022-10-13 21:19:17 +0300

    Unit 3

---
+++ hangman-unit3/hangman-ex3.5.1.py
@@ -0,0 +1,2 @@
+guess_a_letter = input("Guess a letter: ")
+print(guess_a_letter.lower())
---
+++ hangman-unit3/hangman-ex3.5.2.py
@@ -0,0 +1,4 @@
+string = input("Please enter a word: ")
+string_length = len(string)
+new_string =  "_ " * string_length
+print(new_string)
